d693622f8e8a0f97c8b5477ce86ac855520e7a42
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-12 15:47:31 +0900

    📝

--- workspace/ch01-buildup/react/05/src/pages/Todo.jsx
+++ workspace/ch01-buildup/react/05/src/pages/Todo.jsx
@@ -9,7 +9,9 @@ function Todo(props){
           <li>
             <h2>쇼핑 목록</h2>
             <TodoInput addItem={ props.addItem } />
-            <TodoList itemList={ props.itemList } toggleDone={ props.toggleDone } deleteItem={ props.deleteItem } />
+            {/* <TodoList itemList={ props.itemList } toggleDone={ props.toggleDone } deleteItem={ props.deleteItem } /> */}
+            {/* {귀찮아서 propos를 다 전달하기는 했지만 TodoList에서 addItem 함수를 호출해서 item을 추가하면 나중에 상태 관리자가 되지 않아 문제가 될 수 있기 때문에 절대 사용하면 안됨} */}
+            <TodoList {...props}/>
           </li>
         </ul>
       </div>
---
+++ workspace/ch02-start/props/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+  return (
+    <header>
+      <h1>Counter - React</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
