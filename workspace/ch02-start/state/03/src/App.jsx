eda77a2163046b8e659153de7230283c38289a7f
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-14 16:05:55 +0900

    📝

--- workspace/ch02-start/state/03/src/App.jsx
+++ workspace/ch02-start/state/03/src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 function App(){
+
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+
   return (
     <>
       <h1>03 상태관리 대상이 객체일 경우 주의 사항</h1>
       <div
+        onPointerMove={ event => {
+          // 속성을 직접 수ㅜ정하면 리렌더링되지 않음
+          // position.x = event.clientX;
+          // position.y = event.clientY;
+
+          const newPostion = {x : event.clientX, y: event.clientY - 160}
+
+          setPosition(newPostion)
+
+          console.log(position);
+          
+        } }
         style={{
           position: 'relative',
           width: '100vw',
@@ -12,7 +29,7 @@ function App(){
           position: 'absolute',
           backgroundColor: 'red',
           borderRadius: '50%',
-          transform: `translate(${10}px, ${10}px)`,
+          transform: `translate(${position.x-10}px, ${position.y-10}px)`,
           left: -10,
           top: -10,
           width: 20,
