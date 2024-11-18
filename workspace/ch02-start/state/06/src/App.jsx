20064f75f86609c9e66af61d3c1a652785806444
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-18 15:25:52 +0900

    📝

--- workspace/ch02-start/state/06/src/App.jsx
+++ workspace/ch02-start/state/06/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const errorStyle = {
   fontSize: '12px',
@@ -11,121 +11,60 @@ const cellphoneExp = /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/;
 
 function App() {
 
-  
-
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [cellphone, setCellphone] = useState('010');
-
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    cellphone: '010',
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      cellphone: '010',
+    }
   });
 
-  const [errors, setErrors] = useState({});
-
-  // DOM 객체에 직접 접근하려고 할때
-  const nameElem = useRef(null);
-  const emailElem = useRef(null);
-  const cellphoneElem = useRef(null);
-
-  // const handleNameChange = (event) => {
-  //   setName(event.target.value);
-  // };
-  // const handleEmailChange = (event) => {
-  //   setEmail(event.target.value);
-  // };
-  // const handleCellphoneChange = (event) => {
-  //   setCellphone(event.target.value);
-  // };
-
-  const handleChange = (event) => {
-    const newUser = { 
-      ...user,
-      [event.target.name]: event.target.value,
-    };
-    setUser(newUser);
-  };
-
-  const onSubmit = (event) => {
-    // 브라우저의 기본 동작 취소(submit 동작 취소)
-    event.preventDefault();
-
-    let newErrors;
-    // 입력 메세지 검증 작업
-    if(user.name.trim() === ''){
-      newErrors = {
-        name: { message: '이름을 입력하세요.' }
-      };
-      nameElem.current.focus();
-    }else if(user.name.trim().length < 2){
-      newErrors = {
-        name: { message: '2글자 이상 입력하세요.' }
-      };
-      nameElem.current.focus();
-    }else if(user.email.trim() === ''){
-      newErrors = {
-        email: { message: '이메일을 입력하세요.' }
-      };
-      emailElem.current.focus();
-    }else if(user.cellphone.trim() === ''){
-      newErrors = {
-        cellphone: { message: '휴대폰 번호를 입력하세요.' }
-      };
-      cellphoneElem.current.focus();
-    }else if(emailExp.test(user.email) === false){
-      newErrors = {
-        email: { message: '이메일 양식에 맞지 않습니다.' }
-      };
-      emailElem.current.focus();
-    }else if(cellphoneExp.test(user.cellphone) === false){
-      newErrors = {
-        cellphone: { message: '휴대폰 형식에 맞지 않습니다.' }
-      };
-      cellphoneElem.current.focus();
-    }
-
-    if(newErrors){ // 검증 실패
-      setErrors(newErrors);
-    }else{ // 검증 통과
-      setErrors({});
-      console.log('서버에 전송', user);
-    }
+  // handleSubmit에서 검증을 통과할 경우 호출됨
+  const onSubmit = (user) => {
+    console.log('서버에 전송', user);
   };
 
   return (
     <>
-      <h1>06 회원가입 입력값 검증</h1>
+      <h1>06 회원가입 입력값 검증 (feat. react-hook-form)</h1>
 
-      <form onSubmit={ onSubmit }>
+      <form onSubmit={ handleSubmit(onSubmit) }>
         <label htmlFor="name">이름</label>
         <input 
           id="name"
-          name="name"
-          value={ user.name }
-          onChange={ handleChange }
-          ref={ nameElem }
+          { ...register('name', {
+            required: '이름을 입력하세요.',
+            minLength: {
+              value: 2,
+              message: '2글자 이상 입력하세요.'
+            }
+          }) }
         /><br/>
         <div style={ errorStyle }>{ errors.name?.message }</div>
 
         <label htmlFor="email">이메일</label>
         <input 
           id="email"
-          name="email"
-          value={ user.email }
-          onChange={ handleChange }
-          ref={ emailElem }
+          { ...register('email', {
+            required: '이메일을 입력하세요.',
+            pattern: {
+              value: emailExp,
+              message: '이메일 양식에 맞지 않습니다.'
+            }
+          }) }
         /><br/>
         <div style={ errorStyle }>{ errors.email?.message }</div>
 
         <label htmlFor="cellphone">휴대폰</label>
         <input 
           id="cellphone"
-          name="cellphone"
-          value={ user.cellphone }
-          onChange={ handleChange }
-          ref={ cellphoneElem }
+          { ...register('cellphone', {
+            required: '전화번호를 입력하세요.',
+            pattern: {
+              value: cellphoneExp,
+              message: '전화번호 양식에 맞지 않습니다.'
+            }
+          }) }
         /><br/>
         <div style={ errorStyle }>{ errors.cellphone?.message }</div>
 
@@ -133,9 +72,9 @@ function App() {
       </form>
 
       <p>
-        이름: { user.name }<br/>
-        이메일: { user.email }<br/>
-        휴대폰: { user.cellphone }<br/>
+        이름: { watch('name') }<br/>
+        이메일: { watch('email') }<br/>
+        휴대폰: { watch('cellphone') }<br/>
       </p>
     </>
   );
