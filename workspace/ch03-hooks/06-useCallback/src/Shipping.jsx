477afd82ed376f5fdf78cedbcc7bb5244b2aea49
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-21 11:53:57 +0900

    📝

--- workspace/ch03-hooks/06-useCallback/src/Shipping.jsx
+++ workspace/ch03-hooks/06-useCallback/src/Shipping.jsx
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types";
+import { memo } from "react";
 
-Shipping.proptypes ={
-  fees: PropTypes.number.isRequired,
-  handlePayment:PropTypes.func.isRequired,
-}
-
-function Shipping({ fees, handlePayment }) {
+const Shipping = memo(function Shipping({ fees, handlePayment }){
   return (
     <>
       <h2>배송비</h2>
       <div>
-      배송비: { fees.toLocaleString() }원<br/>
+        배송비: { fees.toLocaleString() }원<br/>
       </div>
       <br/>
-      <button type="button" onClick={handlePayment}>결제</button>
+      <button type="button" onClick={ handlePayment } >결제</button>
     </>
-  )
-}
+  );
+});
+
+Shipping.propTypes = {
+  fees: PropTypes.number.isRequired,
+  handlePayment: PropTypes.func.isRequired,
+};
 
 export default Shipping;
