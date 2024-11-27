844d4c0e427b19e4dc328c1129d7c2f06d959a7d
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-27 15:45:09 +0900

    📝

--- workspace/ch06-contextapi/after/src/main.jsx
+++ workspace/ch06-contextapi/after/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SimpleContext } from '@context/SimpleContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+  <SimpleContext.Provider value = {{hello:'world'}}>
+  <App />
+  </SimpleContext.Provider>
+    
   // </StrictMode>
 )
