91e175677b6110a4ef5d9e509ba8e85eeb121409
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-27 15:02:03 +0900

    🚧

--- workspace/ch06-contextapi/after/src/App.jsx
+++ workspace/ch06-contextapi/after/src/App.jsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Left1 from '@components/Left1';
 import Right1 from '@components/Right1';
+import { CounterProvider } from '@context/CounterContext';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const countUp = function(step){
-    setCount(count + step);
-  };
 
   useEffect(()=>{
     console.log('# App 렌더링.');
@@ -15,12 +11,14 @@ function App() {
 
   return (
     <>
-      <h1>Context API - Sample</h1>
+      <h1>Props Drilling 대신 Context API 사용</h1>
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 />
-          <Right1 />
+          <CounterProvider>
+            <Left1 />
+            <Right1 />
+          </CounterProvider>
         </div>
       </div>
     </>
