dd16e245f38ba8aa760fdd2ada4c692eebe9b4a1
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-27 14:25:44 +0900

    📝 🐛

--- workspace/ch06-contextapi/after/src/App.jsx
+++ workspace/ch06-contextapi/after/src/App.jsx
@@ -3,16 +3,8 @@ import Left1 from '@components/Left1';
 import Right1 from '@components/Right1';
 
 function App() {
-  // Left3에 전달
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(0);
 
-  // Right3에 전달
-  const countDown = function(step){
-    setCount(count - step);
-  };
-  const reset = function(){
-    setCount(0);
-  };
   const countUp = function(step){
     setCount(count + step);
   };
@@ -23,12 +15,12 @@ function App() {
 
   return (
     <>
-      <h1>Context API - Props Drilling</h1>
+      <h1>Context API - Sample</h1>
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 count={ count } />
-          <Right1 countDown={ countDown } reset={ reset } countUp={ countUp } />
+          <Left1 />
+          <Right1 />
         </div>
       </div>
     </>
