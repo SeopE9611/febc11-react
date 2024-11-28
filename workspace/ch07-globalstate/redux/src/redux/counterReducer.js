2008d0e24c4f7bcbfe7dc6cb826e9ef8dd7c948b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-28 11:51:43 +0900

    📝

--- workspace/ch07-globalstate/redux/src/redux/counterReducer.js
+++ workspace/ch07-globalstate/redux/src/redux/counterReducer.js
@@ -2,7 +2,6 @@ import { COUNTER_ACTION } from "@redux/counterActionCreator";
 import { cloneElement } from "react";
 
 // 초기 상태
-const initialState = { count: 5 };
 const initialState = { count: 0, hello: "world" };
 
 // 현재 상태와 action 객체를 받아서 새로운 상태를 반환하는 순수 함수
