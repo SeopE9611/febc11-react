f13ec32cc40e6367018a9d760d1d8453e4cf32c9
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-03 11:08:05 +0900

    ✨ 기능 및 코드 추가

--- workspace/ch08-css/04.tailwind/src/App.jsx
+++ workspace/ch08-css/04.tailwind/src/App.jsx
@@ -1,19 +1,21 @@
+import Button from '@components/Button';
 import Login from './Login';
-import Button from './components/Button';
 
-// 첫번째 방법 : import 해서 css 사용
 function App(){
   return (
     <>
       <h1>04 TailWind CSS 사용</h1>
-      <div className='container'>
-      <Button>그냥 버튼</Button>
-      <Button bg='blue' color='red'>파란 배경의 빨간 글자</Button>
-      <Button bg='yellow' color='red'>노란 배경의 빨간 글자</Button>
-      <Button bg='gray' color='blue'>회색 배경의 파란 글자</Button>
-      </div>
 
-      <Login/>
+      <div className="w-52 mt-2 mx-auto">
+        <Button>그냥 버튼</Button>
+        <Button bg="blue" color="red">파란 배경의 빨간 글자</Button>
+        <Button bg="yellow" color="red">노란 배경의 빨간 글자</Button>
+        <Button bg="gray" color="blue">회색 배경의 파란 글자</Button>
+        <button className='card'>버튼</button>
+      </div>
+      
+      <Login />
+
     </>
   );
 }
