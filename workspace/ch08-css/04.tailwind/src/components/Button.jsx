a195f490e2a3e5e6081b3f57557d979f0761cf7e
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-03 10:20:10 +0900

    ✨ 기능 및 코드 추가

--- workspace/ch08-css/04.tailwind/src/components/Button.jsx
+++ workspace/ch08-css/04.tailwind/src/components/Button.jsx
@@ -1,14 +1,33 @@
 import PropTypes from 'prop-types'
-import './Button.css'
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   type: PropTypes.string,
-  color: PropTypes.oneOf(['blue','red','yellow','gray']), // 글자색은 이 4개중 하나 사용 - oneOf
+  color: PropTypes.oneOf(['blue','red','black','white']), // 글자색은 이 4개중 하나 사용 - oneOf
   bg: PropTypes.oneOf(['blue','red','yellow','gray']), // 배경색은 이 4개중 하나 사용 - oneOf
+  size: PropTypes.oneOf(['sm','md','lg']),
   onClick: PropTypes.func, // 클릭이벤트 대비
 }
-export default function Button({ children, type='button', bg, color, onClick:clickHandler }) {
-  return <button className={`button color-${bg}-${color}`} type={type} onClick={clickHandler}>{children}</button>
+export default function Button({ children, bg="gray", color="black", size="md", type='button', onClick:clickHandler }) {
+  let bgColor = {
+    gray: 'bg-gray-400',
+    blue: 'bg-blue-400',
+    red: 'bg-red-400',
+    yellow: 'bg-yellow-400',
+  }
+
+  let textColor = {
+    black:'text-black',
+    white:'text-white',
+    blue:'text-blue-500',
+    red:'text-red-500',
+  }
+
+  let btnSize = {
+    sm :'py-1 px-2 text-sm',
+    md :'py-2 px-4 text-base',
+    lg :'py-2 px-6 text-lg',
+  }
+  return <button className={`${bgColor[bg]} ${textColor[color]} ${btnSize[size]} m-1 rounded-md`} type={type} onClick={clickHandler}>{children}</button>
   
 }
