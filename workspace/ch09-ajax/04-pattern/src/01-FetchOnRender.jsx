f4935838bba76969589b97d5ea126da3bfa11d45
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-05 14:32:35 +0900

    ✨ 기능 및 코드 추가

--- workspace/ch09-ajax/04-pattern/src/01-FetchOnRender.jsx
+++ workspace/ch09-ajax/04-pattern/src/01-FetchOnRender.jsx
@@ -25,6 +25,38 @@ function FetchOnRender() {
   return(
     <>
       <h4>{data.item.title}</h4>
+      <Replies/>
+    </>
+  )
+}
+
+// 댓글 목록 조회 API 호출
+function FetchReplies(){
+  return axios.get('https://11.fesp.shop/posts/1/replies',{
+    headers:{
+      'client-id':'00-brunch'
+    }
+  })
+}
+
+//댓글 목록 페이지
+function Replies() {
+  const [data, setData] = useState();
+  useEffect(()=> {
+    FetchReplies().then((res) => {
+      setData(res.data);
+    }); 
+  },[]);
+  
+  if(!data){ // 만약에 data가 false 일경우
+    return <div>댓글 로딩중...</div>;
+  }
+
+  const list = data.item.map(item => <li key={item._id}>{item.content}</li>);
+
+  return(
+    <>
+      <ul>{list}</ul>
     </>
   )
 }
