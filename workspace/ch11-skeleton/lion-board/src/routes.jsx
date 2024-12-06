9de7649901a84b521d49012d4cf439e0d470d947
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-06 15:41:39 +0900

    ✨

--- workspace/ch11-skeleton/lion-board/src/routes.jsx
+++ workspace/ch11-skeleton/lion-board/src/routes.jsx
@@ -22,6 +22,16 @@ const router = createBrowserRouter([
       { path: 'users/login', element: <Login /> },
     ]
   },
-]);
+],{
+  future: {
+    // 없으면 콘솔에 경고 표시
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+}
+);
 
 export default router;
